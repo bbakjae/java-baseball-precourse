321feb182027bdb9e20b6ffde7e05e309747791b
bbakjae <qkrwogus0724@naver.com>
2020-11-30 15:36:11 +0900

    Final

--- java-baseball-precourse-bbakjae/src/main/java/baseball/Application.java
+++ java-baseball-precourse-bbakjae/src/main/java/baseball/Application.java
@@ -7,6 +7,24 @@ public class Application {
         final Scanner scanner = new Scanner(System.in);
         
         // TODO 구현 진행
+        Computer com = new Computer();
+        User user = new User();
+        Game game = new Game();
+
+        int[] rand_num = com.make_Random_3Num();
         
+        while(true) {        	
+        	System.out.print("숫자를 입력해주세요 : ");
+        	int[] guess = user.get_User_Guess(scanner.nextInt());        	
+        	int[] SnB = game.check_SnB(rand_num, guess);
+        	if (game.print_result(SnB)) {
+        		System.out.println("3개의 숫ㅈ자를 모두 맞히셨습니다! 게임 종료");
+        		break;
+        	}
+        }
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        if (scanner.nextInt() == 1) {
+        	main(args);
+        }
     }
 }
--- java-baseball-precourse-bbakjae/src/main/java/baseball/Game.java
+++ java-baseball-precourse-bbakjae/src/main/java/baseball/Game.java
@@ -5,15 +5,36 @@ import java.util.Scanner;
 import baseball.*;
 
 public class Game {
-	public Game(Scanner scanner) {
-		
+	
+	public boolean print_result(int[] SnB) {
+		if (SnB[0]!=0 && SnB[1]!=0) {
+			System.out.printf("%d볼  %d스트라이크\n",SnB[1],SnB[0]);
+			return false;
+		}
+		else if (SnB[0]==3) {
+			System.out.println(""+SnB[0]+"스트라이크");
+			return true;			
+		}
+		else if (SnB[0]!=0 && SnB[1]==0) {
+			System.out.println(""+SnB[0]+"스트라이크");
+			return false;
+		}
+		else if (SnB[1]!=0 && SnB[0]==0) {
+			System.out.println(""+SnB[1]+"볼");
+			return false;
+		}
+		else if (SnB[0]==0&&SnB[1]==0) {
+			System.out.println("낫싱");
+			return false;
+		}
+		return false;
 	}
 	
 	public int[] check_SnB(int[] rand_num,int[] guess) {
 		int[] Strike_n_Ball = {0,0};
 		
-		Strike_n_Ball[0]=check_Ball(rand_num, guess);
-		Strike_n_Ball[1]=check_Strike(rand_num, guess);
+		Strike_n_Ball[0]=check_Strike(rand_num, guess);
+		Strike_n_Ball[1]=check_Ball(rand_num, guess);
 				
 		return Strike_n_Ball;
 	}
@@ -33,13 +54,13 @@ public class Game {
 		int ball=0;
 		
 		for (int i = 0; i < 3; i++) {
-			if (i!=0 && rand_num[0]==rand_num[i]) {
+			if (i!=0 && rand_num[0]==guess[i]) {
 				ball++;
 			}
-			else if (i!=1 && rand_num[1]==rand_num[i]) {
+			else if (i!=1 && rand_num[1]==guess[i]) {
 				ball++;
 			}
-			else if (i!=2 && rand_num[2]==rand_num[i]) {
+			else if (i!=2 && rand_num[2]==guess[i]) {
 				ball++;
 			}
 		}		
